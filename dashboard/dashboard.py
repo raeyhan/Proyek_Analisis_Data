6858cb98248a4728d44670408867bbc80d47829d
raeyhan <ichmalrrayhan@gmail.com>
2024-07-20 12:24:35 +0700

    revision

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load data
-dongsiDf = pd.read_csv("main_data.csv")  
+dongsiDf = pd.read_csv("PRSA_Data_Dongsi_20130301-20170228.csv")  
 
 # Convert date columns
 years = dongsiDf['year'].values
